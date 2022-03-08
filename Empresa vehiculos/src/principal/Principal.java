d7a1643ee96a07088bd469125c315d3a25155673
Luis <lcgascon221@gmail.com>
2022-03-08 13:43:20 +0100

    Correcion del paquete principal

--- Empresa vehiculos/src/principal/Principal.java
+++ Empresa vehiculos/src/principal/Principal.java
@@ -1,16 +1,17 @@
 package principal;
-
-import java.util.GregorianCalendar;
-import interfazUsuario.*;
-
 import clases.*;
 import exceptions.*;
-
+import interfazUsuario.*;
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		String [] opc= {"1- Hacer una modificacion","2- Alquilar un vehículo","3- Devolver un vehículo alquilado"};
+		String [] opciones= {"1","2","3"};
 		
-		
+		interfazUsuario.Menu.menuOpciones(opc,opciones,"Empresa vehiculos","*");
+
+	}
+
 }
